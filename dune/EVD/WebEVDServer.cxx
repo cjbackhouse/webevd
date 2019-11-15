c685a7df64decb65febb7830b91c22cbb9996fcf
Christopher Backhouse <bckhouse@fnal.gov>
2019-11-15 14:35:06 -0600

    Remove a few unneeded headers.

--- dune/EVD/WebEVDServer.cxx
+++ dune/EVD/WebEVDServer.cxx
@@ -42,13 +42,12 @@ getInputTags(const art::Event& evt)
 #include <string>
 
 #include "fhiclcpp/ParameterSet.h"
-#include "art/Framework/Core/ModuleMacros.h"
-#include "art/Framework/Core/EDAnalyzer.h"
-#include "art/Framework/Principal/Event.h"
 #include "art/Framework/Principal/Handle.h"
 #include "art_root_io/TFileService.h"
-#include "art/Framework/Services/Registry/ServiceHandle.h"
 #include "canvas/Persistency/Common/Ptr.h"
+
+#include "art/Framework/Principal/Event.h"
+#include "gallery/Event.h"
 
 #include "lardataobj/RecoBase/Hit.h"
 #include "lardataobj/RecoBase/SpacePoint.h"
@@ -61,10 +60,6 @@ getInputTags(const art::Event& evt)
 #include "lardataobj/RawData/raw.h" // Uncompress()
 
 #include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
-
-#include "gallery/Event.h"
-
-#include <png.h>
 
 namespace evd
 {

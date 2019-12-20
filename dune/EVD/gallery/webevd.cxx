841ebdd771c3dd0ca5deddb4ec77d0319fa08585
Christopher Backhouse <bckhouse@fnal.gov>
2019-12-20 07:58:33 -0600

    Make use of ArtServiceHelper from gallery to get correctly-configured Geometry and DetectorProperties

--- dune/EVD/gallery/webevd.cxx
+++ dune/EVD/gallery/webevd.cxx
@@ -5,15 +5,10 @@
 #include "canvas/Utilities/InputTag.h"
 #include "gallery/Event.h"
 
-#include "larcorealg/Geometry/GeometryCore.h"
-#include "larcorealg/Geometry/GeometryBuilderStandard.h"
-#include "dune/Geometry/DuneApaChannelMapAlg.h"
-#include "larcorealg/Geometry/GeoObjectSorterStandard.h"
-#include "dune/Geometry/GeoObjectSorterAPA.h"
-
-#include "lardataalg/DetectorInfo/DetectorProperties.h"
-
-#include "lardataalg/DetectorInfo/DetectorPropertiesStandard.h"
+#include "dune/ArtSupport/ArtServiceHelper.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "larcore/Geometry/Geometry.h"
+#include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
 
 #include "dune/EVD/WebEVDServer.h"
 
@@ -58,73 +53,29 @@ int main(int argc, char** argv)
     argv += 2;
   }
 
+  const std::vector<std::string> filenames(argv + 1, argv + argc);
+
+  // Dump the input file geometry configuration. Don't know how to do this in
+  // code yet
+  system(("config_dumper -S -f Geometry "+filenames[0]).c_str());
+
+
   evd::WebEVDServer<gallery::Event> server;
 
-  fhicl::ParameterSet pset;
-  pset.put("SurfaceY", 0);
-  pset.put("Name", "dummy");
-  geo::GeometryCore geom(pset);
+  // TODO why is it necessary to manually specify the GDML?
+  // TODO can we find a way to extract it from the job configuration stored in the file?
+  ArtServiceHelper::load_services(
+"#include \"services_dune.fcl\"            \n"
+"services:{@table::dunefd_services}        \n"
+"services.BackTrackerService: @erase       \n"
+"services.PhotonBackTrackerService: @erase \n"
+"services.Geometry.GDML: \"dune10kt_v1_1x2x6.gdml\" "
+);
 
-  geo::GeometryBuilderStandard::Config cfg;
-  geo::GeometryBuilderStandard builder(cfg);
-  // TODO extract the gdml path from the input file
-  geom.LoadGeometryFile("dummy", "/dune/app/users/bckhouse/dev/evd/build_slf6.x86_64/dunetpc/gdml/dune10kt_v1_1x2x6.gdml", builder);
+  const geo::GeometryCore* geom = art::ServiceHandle<geo::Geometry>()->provider();
+  const detinfo::DetectorProperties* detprop = art::ServiceHandle<detinfo::DetectorPropertiesService>()->provider();
 
-  pset.put("ChannelsPerOpDet", 0);
-  const geo::GeoObjectSorterAPA sorter(pset);
-  std::shared_ptr<geo::DuneApaChannelMapAlg> cmap(new geo::DuneApaChannelMapAlg(pset));
-  cmap->setSorter(sorter);
-  geom.ApplyChannelMap(cmap);
-
-  //  detinfo::DetectorPropertiesStandard detprop;
-  // lar::DetectorPropertiesStandard::providers_type providers;
-  // providers.set(lar::providerFrom<geo::Geometry>());
-  // providers.set(lar::providerFrom<detinfo::LArPropertiesService>());
-  // providers.set(lar::providerFrom<detinfo::DetectorClocksService>());
-  // detprop->Setup(providers);
-
-  // I haven't been able to figure out how to make a properly configured
-  // DetectorProperties. For now just mock one up with the only function we
-  // actually use.
-  struct DummyProperties: public detinfo::DetectorProperties
-  {
-    double ConvertTicksToX(double ticks, const geo::PlaneID& planeid) const override {return 0.0802814*ticks;}
-
-    double Efield(unsigned int planegap=0) const override {abort();}
-    double DriftVelocity(double efield=0., double temperature=0.) const override {abort();}
-    double BirksCorrection(double dQdX) const override {abort();}
-    double ModBoxCorrection(double dQdX) const override {abort();}
-    double ElectronLifetime() const override {abort();}
-    double Density(double temperature) const override {abort();}
-    double Temperature() const override {abort();}
-
-    double Eloss(double mom, double mass, double tcut) const override {abort();}
-    double ElossVar(double mom, double mass) const override {abort();}
-    double       SamplingRate()      const override {abort();}
-    double       ElectronsToADC()    const override {abort();}
-    unsigned int NumberTimeSamples() const override {abort();}
-    unsigned int ReadOutWindowSize() const override {abort();}
-    int          TriggerOffset()     const override {abort();}
-    double       TimeOffsetU()       const override {abort();}
-    double       TimeOffsetV()       const override {abort();}
-    double       TimeOffsetZ()       const override {abort();}
-    double       ConvertXToTicks(double X, int p, int t, int c) const override {abort();}
-    double       ConvertXToTicks(double X, geo::PlaneID const& planeid) const override {abort();}
-    double       ConvertTicksToX(double ticks, int p, int t, int c) const override {abort();}
-    double       GetXTicksOffset(int p, int t, int c) const override {abort();}
-    double       GetXTicksOffset(geo::PlaneID const& planeid) const override {abort();}
-    double       GetXTicksCoefficient() const override {abort();}
-    double       GetXTicksCoefficient(int t, int c) const override {abort();}
-    double       GetXTicksCoefficient(geo::TPCID const& tpcid) const override {abort();}
-    double       ConvertTDCToTicks(double tdc) const override {abort();}
-    double       ConvertTicksToTDC(double ticks) const override {abort();}
-    bool SimpleBoundary()     const override {abort();}
-  };
-
-  const DummyProperties detprop;
-
-
-  const std::vector<std::string> filenames(argv + 1, argv + argc);
+  std::cout << geom->GDMLFile() << std::endl;
 
   for(gallery::Event evt(filenames); !evt.atEnd(); evt.next()){
     const art::EventAuxiliary& aux = evt.eventAuxiliary();
@@ -133,7 +84,7 @@ int main(int argc, char** argv)
        (tgt_subrun >= 0 && aux.subRun() != tgt_subrun) ||
        (tgt_evt >= 0 && aux.event() != tgt_evt)) continue;
 
-    server.analyze(evt, &geom, &detprop);
+    server.analyze(evt, geom, detprop);
 
     std::cout << "\nDisplaying event " << aux.run() << ":" << aux.subRun() << ":" << aux.event() << std::endl << std::endl;
 

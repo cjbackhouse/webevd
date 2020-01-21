abd0476b5f8bf73175e00bfdd1eb5614bd1519b7
Christopher Backhouse <bckhouse@fnal.gov>
2020-01-21 11:59:31 -0600

    Allow geometry to be configured on the gallery app command line, and also auto-detected.

--- dune/EVD/gallery/webevd.cxx
+++ dune/EVD/gallery/webevd.cxx
@@ -14,7 +14,7 @@
 
 void usage()
 {
-  std::cout << "Usage: webevd [-e [[RUN:]SUBRUN:]EVT] events.root [more_events.root...]" << std::endl;
+  std::cout << "Usage: webevd [-d DET] [-e [[RUN:]SUBRUN:]EVT] events.root [more_events.root...]" << std::endl;
   exit(1);
 }
 
@@ -22,57 +22,110 @@ void usage()
 int main(int argc, char** argv)
 {
   if(argc == 1) usage();
+  --argc;
+  ++argv;
+
+  bool detKnown = false;
+  bool isFD = false;
 
   int tgt_run = -1, tgt_subrun = -1, tgt_evt = -1;
-  if(argc >= 2 && std::string(argv[1]) == "-e"){
-    if(argc <= 3) usage();
 
-    std::vector<int> toks;
+  while(argc >= 1 && argv[0][0] == '-'){
+    if(std::string(argv[0]) == "-d"){
+      if(argc < 2) usage();
 
-    char* ptok = strtok(argv[2], ":");
-    while(ptok){
-      toks.push_back(atoi(ptok));
-      ptok = strtok(0, ":");
+      const std::string d(argv[1]);
+
+      detKnown = true;
+
+      if(d == "fd" || d == "fardet" || d == "dune10kt") isFD = true;
+      else if(d == "pd" || d == "protodune" || d == "np04") isFD = false;
+      else{
+        std::cout << "Unrecognized detector '" << d << "'" << std::endl;
+        return 1;
+      }
+
+      argc -= 2;
+      argv += 2;
     }
 
-    if(toks.empty() || toks.size() > 3) usage();
+    if(std::string(argv[0]) == "-e"){
+      if(argc < 2) usage();
 
-    tgt_evt = toks[toks.size()-1];
-    std::cout << "Will look for event " << tgt_evt;
-    if(toks.size() > 1){
-      tgt_subrun = toks[toks.size()-2];
-      std::cout << " in subrun " << tgt_subrun;
+      std::vector<int> toks;
+
+      char* ptok = strtok(argv[1], ":");
+      while(ptok){
+        toks.push_back(atoi(ptok));
+        ptok = strtok(0, ":");
+      }
+
+      if(toks.empty() || toks.size() > 3) usage();
+
+      tgt_evt = toks[toks.size()-1];
+      std::cout << "Will look for event " << tgt_evt;
+      if(toks.size() > 1){
+        tgt_subrun = toks[toks.size()-2];
+        std::cout << " in subrun " << tgt_subrun;
+      }
+      if(toks.size() > 2){
+        tgt_run = toks[toks.size()-3];
+        std::cout << " in run " << tgt_run;
+      }
+      std::cout << std::endl;
+
+      argc -= 2;
+      argv += 2;
     }
-    if(toks.size() > 2){
-      tgt_run = toks[toks.size()-3];
-      std::cout << " in run " << tgt_run;
+  } // end while options remain
+
+  const std::vector<std::string> filenames(argv, argv + argc);
+
+  if(!detKnown){
+    if(filenames[0].find("dune10kt") != std::string::npos ||
+       filenames[0].find("1x2x6") != std::string::npos){
+      isFD = true;
+      detKnown = true;
     }
+    else if(filenames[0].find("np04") != std::string::npos){
+      isFD = false;
+      detKnown = true;
+    }
+    else{
+      std::cout << "Unable to auto-detect detector from filename. Please specify it explicitly with -d" << std::endl;
+      return 1;
+    }
+
+    std::cout << "Auto-detected geometry as ";
+    if(isFD) std::cout << "far detector"; else std::cout << "ProtoDUNE SP";
     std::cout << std::endl;
-
-    argc -= 2;
-    argv += 2;
   }
 
-  const std::vector<std::string> filenames(argv + 1, argv + argc);
-
-  // Dump the input file geometry configuration. Don't know how to do this in
-  // code yet
-  system(("config_dumper -S -f Geometry "+filenames[0]).c_str());
-
+  // Prototype for automatically configuring the geometry below. Dumps the
+  // input file geometry configuration. Don't know how to do this in code yet
+  // system(("config_dumper -S -f Geometry "+filenames[0]).c_str());
 
   evd::WebEVDServer<gallery::Event> server;
 
-  // TODO why is it necessary to manually specify the GDML?
-  // TODO can we find a way to extract it from the job configuration stored in the file?
-  ArtServiceHelper::load_services(
-"#include \"services_dune.fcl\"            \n"
-"services:{@table::dunefd_services}        \n"
-"services.BackTrackerService: @erase       \n"
-"services.PhotonBackTrackerService: @erase \n"
-"services.LArFFT: @erase                   \n"
-"services.TFileService: @erase             \n"
-"services.Geometry.GDML: \"dune10kt_v1_1x2x6.gdml\" "
-);
+  std::string fclConfig = "#include \"services_dune.fcl\"\n";
+  if(isFD){
+    fclConfig +=
+      "services:{@table::dunefd_services}\n"
+      "services.Geometry.GDML: \"dune10kt_v1_1x2x6.gdml\"\n";
+    // TODO why is it necessary to manually specify the GDML?
+  }
+  else{
+    fclConfig +=
+      "services:{@table::protodune_services}\n";
+  }
+
+  fclConfig +=
+    "services.BackTrackerService: @erase       \n"
+    "services.PhotonBackTrackerService: @erase \n"
+    "services.LArFFT: @erase                   \n"
+    "services.TFileService: @erase             \n";
+
+  ArtServiceHelper::load_services(fclConfig);
 
   const geo::GeometryCore* geom = art::ServiceHandle<geo::Geometry>()->provider();
   const detinfo::DetectorProperties* detprop = art::ServiceHandle<detinfo::DetectorPropertiesService>()->provider();

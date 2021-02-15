b89e30bdb4e75f481d67cdb9bda9b388db35ded5
Christopher Backhouse <bckhouse@fnal.gov>
2021-02-15 17:49:17 -0600

    Trivial fix to browser executable

--- webevd/WebEVD/gallery/webevd_browser.cxx
+++ webevd/WebEVD/gallery/webevd_browser.cxx
@@ -2,10 +2,12 @@
 
 #include "webevd/WebEVD/WebEVDServer.h"
 
+#include "webevd/WebEVD/ThreadsafeGalleryEvent.h"
+
 // We use a function try block to catch and report on all exceptions.
 int main(int argc, char** argv)
 {
-  evd::WebEVDServer<gallery::Event> server;
+  evd::WebEVDServer<evd::ThreadsafeGalleryEvent> server;
   server.serve_browser();
   return 0;
 }
